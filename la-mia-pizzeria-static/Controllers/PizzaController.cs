da26aa3b8cd510375fab9942686b604e666ebb57
Hadson Roque <nineonezero.light@gmail.com>
2023-05-03 16:44:17 +0200

    Esercizio finito

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -7,8 +7,8 @@ namespace la_mia_pizzeria_static.Controllers
         List<Pizza> pizze = new List<Pizza>
             {
                 new Pizza(1, "Pizza Margherita", "Bella buona", "https://www.nicesurgelati.it/wp-content/uploads/2018/05/pizza_margherita_nice.jpg", 6),
-                new Pizza(2, "Pizza Diavola", "Bella buona", "https://www.nicesurgelati.it/wp-content/uploads/2018/05/pizza_margherita_nice.jpg", 8),
-                new Pizza(3, "Pizza Bufala", "Bella buona", "https://www.nicesurgelati.it/wp-content/uploads/2018/05/pizza_margherita_nice.jpg", 8),
+                new Pizza(2, "Pizza Diavola", "Bella piccante bellabuona", "https://www.kennedyfood.it/wp-content/uploads/2019/03/diavola.jpg", 8),
+                new Pizza(3, "Pizza Bufala", "Buon pizza con mozzarella", "https://www.atuttofarro.it/wp-content/uploads/2019/02/atuttofarro_pizzarossa_bufala.jpg", 8),
             };
 
         public IActionResult Index()
@@ -20,11 +20,11 @@ namespace la_mia_pizzeria_static.Controllers
         { 
             var pizza = pizze.FirstOrDefault(p => p.Id == id);
 
-            if (pizza == null)
+            if (pizza == null || id < 1 || id > pizze.Count)
             {
-                return NotFound();
+                return View("ErrorPizza");
             }
-
+            
             return View(pizza);
         }
 
