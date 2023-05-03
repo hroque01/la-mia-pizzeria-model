325a8f8de4b47699903ea4a9b7d4d88ba2e2818e
Hadson Roque <nineonezero.light@gmail.com>
2023-05-03 16:01:57 +0200

    Create PizzaController & Class Pizza with List

---
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza> pizze = new List<Pizza>
+            {
+                new Pizza("Pizza Margherita", "Bella buona", "https://www.nicesurgelati.it/wp-content/uploads/2018/05/pizza_margherita_nice.jpg", 6),
+                new Pizza("Pizza Diavola", "Bella buona", "https://www.nicesurgelati.it/wp-content/uploads/2018/05/pizza_margherita_nice.jpg", 8),
+                new Pizza("Pizza Bufala", "Bella buona", "https://www.nicesurgelati.it/wp-content/uploads/2018/05/pizza_margherita_nice.jpg", 8),
+            };
+            return View(pizze);
+        }
+
+    }
+}
