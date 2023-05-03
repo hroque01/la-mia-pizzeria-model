28ccf446adbe7e721082ec2f1231cdc21ec04257
Hadson Roque <nineonezero.light@gmail.com>
2023-05-03 16:10:31 +0200

    Add details of pizza.Id

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -4,15 +4,28 @@ namespace la_mia_pizzeria_static.Controllers
 {
     public class PizzaController : Controller
     {
-        public IActionResult Index()
-        {
-            List<Pizza> pizze = new List<Pizza>
+        List<Pizza> pizze = new List<Pizza>
             {
-                new Pizza("Pizza Margherita", "Bella buona", "https://www.nicesurgelati.it/wp-content/uploads/2018/05/pizza_margherita_nice.jpg", 6),
-                new Pizza("Pizza Diavola", "Bella buona", "https://www.nicesurgelati.it/wp-content/uploads/2018/05/pizza_margherita_nice.jpg", 8),
-                new Pizza("Pizza Bufala", "Bella buona", "https://www.nicesurgelati.it/wp-content/uploads/2018/05/pizza_margherita_nice.jpg", 8),
+                new Pizza(1, "Pizza Margherita", "Bella buona", "https://www.nicesurgelati.it/wp-content/uploads/2018/05/pizza_margherita_nice.jpg", 6),
+                new Pizza(2, "Pizza Diavola", "Bella buona", "https://www.nicesurgelati.it/wp-content/uploads/2018/05/pizza_margherita_nice.jpg", 8),
+                new Pizza(3, "Pizza Bufala", "Bella buona", "https://www.nicesurgelati.it/wp-content/uploads/2018/05/pizza_margherita_nice.jpg", 8),
             };
+
+        public IActionResult Index()
+        { 
             return View(pizze);
+        }
+
+        public IActionResult Details(int id) 
+        { 
+            var pizza = pizze.FirstOrDefault(p => p.Id == id);
+
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            return View(pizza);
         }
 
     }
--- la-mia-pizzeria-static/Pizza.cs
+++ la-mia-pizzeria-static/Pizza.cs
@@ -2,13 +2,15 @@
 {
     public class Pizza
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
         public decimal Price { get; set; }  
 
-        public Pizza(string name, string description, string image, decimal price)
+        public Pizza(int id, string name, string description, string image, decimal price)
         {
+            Id = id;
             Name = name;
             Description = description;
             Image = image;
